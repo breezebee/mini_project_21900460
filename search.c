a9aad89cbc5c3fcd93e3013e69a87e6fb7959aa6
Yoo sumin <tnals8400@naver.com>
2020-04-13 17:02:07 +0900

    Add star number  search func

--- search.c
+++ search.c
@@ -37,6 +37,23 @@ void searchSnackPrice(Snack *s, int count){
 	}
 } 
 void searchSnackStarNum(Snack *s, int count){
+	int scount = 0;
+	int min=0, max=0;
+	printf("검색할 최소, 최대 별 개수를 입력하세요(inclusive)");
+	printf("최소별점: ");
+	scanf("%d", min);
+	printf("최대별점: ");
+	scanf("%d", max);
+	for(int i =0; i<count; i++){
+		if(s[i].starNum >= min && s[i].starNum <= max){
+			readSnack(s[i]);
+			scount++;
+		}
+	}
+	if(scount == 0){
+		printf("검색된 데이터 없음!");
+	}
+} 
 
 }
 
